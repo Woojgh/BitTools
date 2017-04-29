06a5abdb9be416500b31fc760baea755a9e77648
Woojgh <jamessalamonsen@gmail.com>
2017-04-29 11:48:34 -0700

    got boxes to work better.

--- public/scripts/widget.js
+++ public/scripts/widget.js
@@ -4,14 +4,15 @@ function addInputs(choices) {
   result.innerHTML = '';
   for (var i = 0; i < parseInt(choices.value); i++) {
     var wrapper = document.createElement('div');
-    wrapper.innerHTML = '<input type="text" placeholder="choice ' + i + '" /> <input class="fill-color" type="color" name="barfillcolor">';
+    wrapper.innerHTML = '<input class="fillcheckbox" type="checkbox"> <input class="choice-input" type="text"  style="margin:1vw;" placeholder="choice ' + i + '" /> <input class="fill-color" type="color" name="barfillcolor">';
     result.appendChild(wrapper);
   }
 }
 $('#poll-choices').on('change', function () {
 	addInputs(this);
 });
-$('#fillcheckbox').on('click', function () {
+
+$('#fillcheckboxall').on('click', function () {
 	$('.fill-color').toggle();
 });
 $('#test-button').on('click', function (){
