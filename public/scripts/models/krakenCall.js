7d9e25b9de3c4328cd4cab63426a8facb3a336ab
Woojgh <jamessalamonsen@gmail.com>
2017-04-28 20:59:37 -0700

    I got the code chatting to the bot on twitch, now just seeing how to get the information we need from the tmi.js.

---
+++ public/scripts/models/krakenCall.js
@@ -0,0 +1,30 @@
+var tmi = require('tmi.js');
+
+var options = {
+	options:{
+		debug: true
+	},
+	connection: {
+		cluster: "aws",
+		reconnect: true
+	},
+	identity: {
+		username:"Woojgh",
+		password: "oauth:z87bh5uupjlzydic0n339fch9ek3f5"
+	},
+	channels: ["Woojgh"]
+}
+var client = new tmi.client(options);
+client.connect();
+
+// client.api({
+//     url: "https://api.twitch.tv/kraken/user",
+//     method: "GET",
+//     headers: {
+//         "Accept": "application/vnd.twitchtv.v3+json",
+//         "Authorization": "OAuth 3eb787117110834e079932bedfb8e6a7",
+//         "Client-ID": "1dac77895e8f56fa1a71e7c43ef09d87"
+//     }
+// }, function(err, res, body) {
+//     console.log(body);
+// });
