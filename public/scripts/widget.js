2f1648c1359954b48a8c71823d4ac7ab11bff54b
Woojgh <jamessalamonsen@gmail.com>
2017-04-30 09:29:55 -0700

    got more things working

--- public/scripts/widget.js
+++ public/scripts/widget.js
@@ -47,6 +47,27 @@ var defaultColor = "#0000ff";
 //   colorWell.select();
 // }
 
+$(function(){
+  $(".increment").click(function(){
+    var count = parseInt($("~ .count", this).text());
+    
+    if($(this).hasClass("up")) {
+      var count = count + 1;
+      
+       $("~ .count", this).text(count);
+    } else {
+      var count = count - 1;
+       $("~ .count", this).text(count);     
+    }
+    
+    $(this).parent().addClass("bump");
+    
+    setTimeout(function(){
+      $(this).parent().removeClass("bump");    
+    }, 400);
+  });
+});
+
 $(document).ready(function() {
     $("#choice-result div a").click(function() {
         $(this).parent().animate({
