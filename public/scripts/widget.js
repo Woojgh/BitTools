3a12d9b173961ffa66d1b6a3a2ec056fcc1898fc
Woojgh <jamessalamonsen@gmail.com>
2017-04-29 17:21:12 -0700

    stuffff

--- public/scripts/widget.js
+++ public/scripts/widget.js
@@ -4,17 +4,56 @@ function addInputs(choices) {
   result.innerHTML = '';
   for (var i = 0; i < parseInt(choices.value); i++) {
     var wrapper = document.createElement('div');
-    wrapper.innerHTML = '<input class="fillcheckbox" type="checkbox"> <input class="choice-input" type="text"  style="margin:1vw;" placeholder="choice ' + i + '" /> <input class="fill-color" type="color" name="barfillcolor">';
+    wrapper.innerHTML = '<input class="fillcheckbox" type="checkbox"> <input class="choice-input" type="text"  style="margin:1vw; color:colorValue;" placeholder="choice ' + i + '" /> <input class="fill-color" value="#00FFFF" type="color" name="barfillcolor">';
     result.appendChild(wrapper);
   }
-}
+};
+
+function addPreview(choices) {
+  var result = document.querySelector('#container');
+  result.innerHTML = ' ';
+  for (var i = 0; i < parseInt(choices.value); i++) {
+    var wrapper = document.createElement('div');
+    wrapper.innerHTML = '<span>0</span> <a href=" ">Vote</a>';
+    result.appendChild(wrapper);
+  }
+};
+
 $('#poll-choices').on('change', function () {
 	addInputs(this);
+  addPreview(this);
 });
-
+// $('#poll-choices').on('change', function () {
+//   addPreview(this);
+// })
 $('#fillcheckboxall').on('click', function () {
 	$('.fill-color').toggle();
 });
-$('#test-button').on('click', function (){
-	$('.widget-display').toggle();
+$('#test-button').on('click', function (e){
+  e.preventDefault();
+	$('#widget-display').toggle();
 }); 
+
+var colorWell;
+var defaultColor = "#0000ff";
+
+// window.addEventListener("load", startup, false);
+
+// function startup() {
+//   colorWell = document.querySelector(".fill-color");
+//   colorWell.value = defaultColor;
+//   colorWell.addEventListener("input", updateFirst, false);
+//   colorWell.addEventListener("change", updateAll, false);
+//   colorWell.select();
+// }
+
+$(document).ready(function() {
+    $("#choice-result div a").click(function() {
+        $(this).parent().animate({
+           width: '+=100px'
+        }, 500);
+
+        $(this).prev().html(parseInt($(this).prev().html()) + 1);
+        return false;
+    });
+});
