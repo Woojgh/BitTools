2d76f63610e40d2a0bbd7703d1d554e4a65da1b1
Zachary Diehl <zachary.diehl@gmail.com>
2017-05-02 14:13:49 -0700

    testing oauth token in heroku

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,28 +1,32 @@
 'use strict';
-(function(module) {
-	$('#widget-form').hide();
-	var twitchLogin = {};
-  // var host = document.location.href.split("//")[1].split("/")[0];
+$('#widget-form').hide();
+var host = document.location.href.split("//")[1].split("/")[0];
 
-	Twitch.init({clientId: 'pxic46d4dsydwhxvlh341kb7dgdnc6'}, function(error, status) {
-    if (status.authenticated) {
-  $('.twitch-connect').hide();
-  $('#widget-form').show();
-//   if (document.location.hash != "")
-// 			{
-// 				({ name: "auth", newValue: document.location.hash.split("=")[1].split("&")[0], secure: !isDev });
-// 				({ name: "authchange", newValue: "true" });
-// }
-  }
+Twitch.init({clientId: 'pxic46d4dsydwhxvlh341kb7dgdnc6'}, function(error, status) {
+  if (status.authenticated) {
+$('.twitch-connect').hide();
+$('#widget-form').show();
+
+}
 });
 
-	$('.twitch-connect').click(function () {
-	  Twitch.login({
-	  	redirect_uri: 'https://bittoolscod301.herokuapp.com',
-	    scope: ['user_read', 'channel_read']
-	  });
-	})
-
-  var access
-	module.twitchLogin = twitchLogin;
-})(window);
+$('.twitch-connect').click(function () {
+  Twitch.login({
+  	redirect_uri: 'https://bittoolscod301.herokuapp.com',
+    scope: ['user_read', 'channel_read']
+  });
+    if (document.location.hash != "")
+  			{
+  				var newValue = document.location.hash.split("=")[1].split("&")[0];
+  }
+  $.ajax({
+       url: 'https://api.twitch.tv/kraken',
+       beforeSend: function(xhr) {
+            xhr.setRequestHeader("Accept", "application/vnd.twitchtv.v5+json"),
+            xhr.setRequestHeader("Authorization", "OAuth cfabdegwdoklmawdzdo98xt2fo512y")
+       }, success: function(data){
+           alert(data);
+           //process the JSON data etc
+       }
+})
+})
