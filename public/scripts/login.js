28b0dfd418c4c50248ae291cff38bd177709c512
Zachary Diehl <zachary.diehl@gmail.com>
2017-05-02 13:20:40 -0700

    testing cookies heroku

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -2,18 +2,19 @@
 (function(module) {
 	$('#widget-form').hide();
 	var twitchLogin = {};
-  var host = document.location.href.split("//")[1].split("/")[0];
+  // var host = document.location.href.split("//")[1].split("/")[0];
 
 	Twitch.init({clientId: 'pxic46d4dsydwhxvlh341kb7dgdnc6'}, function(error, status) {
     if (status.authenticated) {
   $('.twitch-connect').hide();
   $('#widget-form').show();
-  if (document.location.hash != "")
-			{
-				({ name: "auth", newValue: document.location.hash.split("=")[1].split("&")[0], secure: !isDev });
-				({ name: "authchange", newValue: "true" });
-}
-})
+//   if (document.location.hash != "")
+// 			{
+// 				({ name: "auth", newValue: document.location.hash.split("=")[1].split("&")[0], secure: !isDev });
+// 				({ name: "authchange", newValue: "true" });
+// }
+  }
+});
 
 	$('.twitch-connect').click(function () {
 	  Twitch.login({
