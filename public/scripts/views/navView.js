ef6bce81a5b397c37106742c750225312c3416fc
Christina Thomas <Penssake@gmail.com>
2017-05-04 17:59:22 -0600

    burger fixins

--- public/scripts/views/navView.js
+++ public/scripts/views/navView.js
@@ -2,8 +2,5 @@
 
 $('#hamburger').on('click', function() {
   $('#nav-bar').width('100%');
-});
-
-$('#hamburgerClose').on('click', function() {
-  $('#nav-bar').width('0%');
+  $('#hamburger').hide();
 });
