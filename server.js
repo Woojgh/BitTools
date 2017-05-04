e0168bba270fa8f96faa0d43d41d143f224528fa
Zachary Diehl <zachary.diehl@gmail.com>
2017-05-03 17:14:36 -0700

    created about and instructions page and wrote functionality to show/hide on click

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 'use strict';
 // #1 define constants and requirements
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const pg = require('pg');
 const tmi = require('tmi.js');
 const app = express();
 const PORT = process.env.PORT || 31337;
 
-const conString = 'postgres://postgres:postgres@localhost:5433/day3'
-// const conString = process.env.DATABASE_URL;
+// const conString = 'postgres://zdiehlio:chimaera1@localhost:31337/day3'
+const conString = process.env.DATABASE_URL;
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
